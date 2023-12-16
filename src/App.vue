0b9be6615af893559d73c59207a57204b20f31ce
Paola Cernada <96987381+paolacernada@users.noreply.github.com>
2023-12-16 10:05:17 -0500

    setup up for development

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
-<script setup>
-</script>
-
 <template>
-  <div>
+  <div class="app-container">
     <h1>Vue Shopping List</h1>
-    <!-- your code here -->
+    <ShoppingList v-for="list in shoppingLists" :key="list.id" :list="list" />
   </div>
 </template>
 
+<script>
+import ShoppingList from './components/ShoppingList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ShoppingList,
+    ListItem
+  },
+  data() {
+    return {
+      shoppingLists: []
+    };
+  },
+  mounted() {
+    // Fetch lists from API
+  }
+};
+</script>
+
 <style scoped>
+.app-container {
+  
+}
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="item-container">
+      {{ item.name }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ListItem',
+    props: ['item']
+  };
+  </script>
+  
+  <style>
+  .item-container {
+    
+  }
+  </style>
+  
